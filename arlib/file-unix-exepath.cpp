059fd55b389f4d8c9bd63aba13816b1920f5c0e8
Alcaro <floating@muncher.se>
2019-05-29 13:43:25 +0200

    commit message

---
+++ arlib/file-unix-exepath.cpp
@@ -0,0 +1,41 @@
+#include "file.h"
+#include "os.h"
+#include "thread.h"
+#include "init.h"
+
+#ifdef __unix__
+#include <unistd.h>
+//#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <errno.h>
+
+static char* g_exepath;
+cstring file::exepath() { return g_exepath; }
+
+oninit()
+{
+	array<char> buf;
+	buf.resize(64);
+	
+again: ;
+	ssize_t r = readlink("/proc/self/exe", buf.ptr(), buf.size());
+	if (r <= 0) abort();
+	if ((size_t)r >= buf.size()-1)
+	{
+		buf.resize(buf.size() * 2);
+		goto again;
+	}
+	
+	buf[r] = '\0';
+	char * end = strrchr(buf.ptr(), '/')+1; // a / is known to exist
+	*end = '\0';
+	
+	g_exepath = buf.release().ptr();
+}
+#endif
